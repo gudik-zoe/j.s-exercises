a30dec5f8693b06b83cf954b895c5dbe9d8f95d5
gudik-zoe <tony_khury993@hotmail.com>
2020-01-31 09:37:05 +0100

    java exercises

---
+++ script.js
@@ -0,0 +1,49 @@
+// // /* this is a comment */
+//alert('hello , world!')//
+
+// const firstName = 'tony'
+// const lastName = 'khoury'
+// const fullName = firstName + ' ' + lastName
+// alert(fullName)
+
+//  const x = 4
+//  const y = 5
+//  const z = x + y
+// alert(z)
+
+
+// const myAdress = 'via bogliette 20 \nsecond floor \nantoine bell'
+// alert(myAdress)
+
+// const streetName = 'via bogliette';
+//  const civicNumber = '20';
+//  const zipCode = '10064';
+//  const city = 'pinerolo';
+//  const province = 'TO';
+//  const country = 'italy';
+//  const newAdress = `${streetName} ${civicNumber}
+// ${zipCode} ${city} (${province})
+// ${country}`
+//  alert(newAdress)
+ 
+const fullName = `name and surename: 
+Antoine Khoury`;
+const myStudies = `studies: 
+physics and math`;
+const workExperience = `work experience: 
+teaching , restaurant services`
+const languageSkills = `language skills:
+ english italian french & spanish`
+const cvResult = `${fullName}
+${myStudies} 
+${workExperience}
+${languageSkills}`
+alert(cvResult)
+
+
+
+
+
+//const power = (x,y,z) => console.log(x*y-z)
+
+
